0c53a89959afaec0adbd4315243c111ef9892548
gladoncio <42326122+gladoncio@users.noreply.github.com>
2024-01-02 01:37:18 -0300

    Update install_dotfiles.sh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -33,35 +33,35 @@ setxkbmap -layout es -variant latam
 # Instala Xorg y utilidades básicas (si no están instaladas)
 sudo pacman -Syu --noconfirm xorg xorg-xinit xorg-server-utils mesa
 
-# Utiliza lspci para obtener información sobre la tarjeta gráfica
-gpu_info=$(lspci | grep VGA)
-echo "Información de la tarjeta gráfica: $gpu_info"
+# # Utiliza lspci para obtener información sobre la tarjeta gráfica
+# gpu_info=$(lspci | grep VGA)
+# echo "Información de la tarjeta gráfica: $gpu_info"
 
-# Determina el controlador de gráficos basándote en la información de lspci
-if [[ $gpu_info == *"Intel"* ]]; then
-    echo "Detectada tarjeta gráfica Intel. Instalando controlador para Intel."
-    sudo pacman -S --noconfirm xf86-video-intel
-elif [[ $gpu_info == *"NVIDIA"* ]]; then
-    echo "Detectada tarjeta gráfica NVIDIA. Instalando controlador para NVIDIA."
-    sudo pacman -S --noconfirm nvidia
-elif [[ $gpu_info == *"AMD"* ]]; then
-    echo "Detectada tarjeta gráfica AMD. Instalando controlador para AMD."
-    sudo pacman -S --noconfirm xf86-video-amdgpu
-elif [[ $gpu_info == *"QEMU"* || $gpu_info == *"VirtualBox"* || $gpu_info == *"VMware"* ]]; then
-    echo "Detectada máquina virtual. Instalando controlador genérico."
-    sudo pacman -S --noconfirm xf86-video-vesa
-else
-    echo "No se pudo determinar automáticamente el controlador de gráficos. Por favor, instálalo manualmente."
-fi
+# # Determina el controlador de gráficos basándote en la información de lspci
+# if [[ $gpu_info == *"Intel"* ]]; then
+#     echo "Detectada tarjeta gráfica Intel. Instalando controlador para Intel."
+#     sudo pacman -S --noconfirm xf86-video-intel
+# elif [[ $gpu_info == *"NVIDIA"* ]]; then
+#     echo "Detectada tarjeta gráfica NVIDIA. Instalando controlador para NVIDIA."
+#     sudo pacman -S --noconfirm nvidia
+# elif [[ $gpu_info == *"AMD"* ]]; then
+#     echo "Detectada tarjeta gráfica AMD. Instalando controlador para AMD."
+#     sudo pacman -S --noconfirm xf86-video-amdgpu
+# elif [[ $gpu_info == *"QEMU"* || $gpu_info == *"VirtualBox"* || $gpu_info == *"VMware"* ]]; then
+#     echo "Detectada máquina virtual. Instalando controlador genérico."
+#     sudo pacman -S --noconfirm xf86-video-vesa
+# else
+#     echo "No se pudo determinar automáticamente el controlador de gráficos. Por favor, instálalo manualmente."
+# fi
 
 
-# Configura Xorg
-echo "Section \"Device\"
-    Identifier  \"Auto-detected Graphics\"
-    Driver      \"auto-detected\"
-EndSection" | sudo tee /etc/X11/xorg.conf.d/20-auto-detected.conf > /dev/null
+# # Configura Xorg
+# echo "Section \"Device\"
+#     Identifier  \"Auto-detected Graphics\"
+#     Driver      \"auto-detected\"
+# EndSection" | sudo tee /etc/X11/xorg.conf.d/20-auto-detected.conf > /dev/null
 
-# Inicia Xorg automáticamente al iniciar sesión en la consola
-echo "exec startx" | sudo tee /root/.bash_profile > /dev/null
+# # Inicia Xorg automáticamente al iniciar sesión en la consola
+# echo "exec startx" | sudo tee /root/.bash_profile > /dev/null
 
-echo "Configuración de controladores de gráficos y Xorg completada con éxito."
+# echo "Configuración de controladores de gráficos y Xorg completada con éxito."
