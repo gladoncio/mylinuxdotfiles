93f33cd2750b7ef6a0923ef7769182bfd553be78
gladoncio <42326122+gladoncio@users.noreply.github.com>
2024-01-01 22:59:27 -0300

    update

--- install_inicial.sh
+++ install_inicial.sh
@@ -56,3 +56,20 @@ pacman -S reflector
 reflector --verbose --latest 10 --sort rate --save /etc/pacman.d/mirrorlist
 
 
+pacman -S grub os-prober ntfs-3g
+
+# Mostrar información sobre los discos disponibles con opciones numeradas
+echo "Discos disponibles:"
+lsblk -d -o NAME,SIZE,TYPE --noheadings | nl -w2 -s') '
+
+# Solicitar al usuario que elija un disco por número
+echo -n "Ingrese el número del disco en el que desea configurar las particiones: "
+read choice
+
+# Obtener el nombre del disco seleccionado
+disk_name=$(lsblk -d -o NAME --noheadings | sed -n "${choice}p")
+
+# Verificar si el disco ingresado es válido
+if [ -n "$disk_name" ]; then
+
+    grub-install "/dev/$disk_name"
