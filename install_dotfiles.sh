2920374036422dbe10287c584f1414d752b35f40
gladoncio <42326122+gladoncio@users.noreply.github.com>
2024-01-02 00:32:38 -0300

    Update install_dotfiles.sh

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -29,7 +29,7 @@ sudo pacman -S xkeyboard-config
 
 setxkbmap -layout es -variant latam
 
-sudo systemctl restart display-manager.service
+#sudo systemctl restart display-manager.service
 
 # Instala Xorg y utilidades básicas (si no están instaladas)
 sudo pacman -Syu --noconfirm xorg xorg-xinit xorg-server-utils mesa
